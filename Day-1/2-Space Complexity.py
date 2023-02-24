b144320f775499af133db7810b90b8b72d107d6b
Fatin Ishraq Prapya <prapyabakshi123@gmail.com>
2023-02-24 09:50:08 +0600

    Space Complexity Done

---
+++ Day-1/2-Space Complexity.py
@@ -0,0 +1,14 @@
+# Space Complexity
+n = int(input())
+# todo: O(1)
+# if n % 2 == 0:
+#     print("Even")
+# else:
+#     print("ODD")
+
+# todo: O(n)
+# arr = [False] * (n + 1)
+# for i in range(0, n+1, 2):
+#     arr[i] = True
+# print(arr)
+# print(len(arr))
