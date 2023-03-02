9ea94d7a1b7245dce9e51e16aa758646a270a43c
Fatin Ishraq Prapya <prapyabakshi123@gmail.com>
2023-03-02 16:51:09 +0600

    Insertion Sort done

---
+++ Day-1/4-Sorting algorithms/3-Insertion Sort.py
@@ -0,0 +1,14 @@
+myArr = [9, 4, 8, 6, 3, 7]
+
+def insertion_sort (arr):
+    length = len(arr)
+    for i in range(1, length):
+        t = arr[i]
+        j = i - 1
+        while j >= 0 and t < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j + 1] = t
+    return arr
+
+print(insertion_sort(myArr))
