4e3474b42d2395bee3a3bb67d34ce9593ff8224e
Fatin Ishraq Prapya <prapyabakshi123@gmail.com>
2023-03-13 23:15:31 +0600

    Bracket Balanced Problem Solved

--- Day-2/2-Bracket Problem Using Stack.py
+++ Day-2/2-Bracket Problem Using Stack.py
@@ -1,15 +1,21 @@
 
-def isBalanced (input_str):
-    s = list()
-    for ch in input_str:
-        if ch == '(':
-            s.append(ch)
-        if ch == ')':
-            if not s:
-                return False
-            s.pop()
-    return not s
+def isBalanced (string):
+    length = len(string)
+    stack = list()
+    for i in range(length):
+        if string[i] == "(":
+            stack.append(string[i])
+        else:
+            if string[i] == ")":
+                if len(stack) == 0:
+                    return False
+                stack.pop()
+    if len(stack) == 0:
+        return True
 
-string = "())"
-if isBalanced(string) == True: print(string+" is balanced")
-else: print(string + " is not balanced")
+inputString = input()
+check = isBalanced(inputString)
+if check == True:
+    print("The String is Balanced")
+else:
+    print("The String is not Balanced")
