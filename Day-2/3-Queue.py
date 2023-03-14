2e58f04beb31a0813b9ef0fe6918920f4e987953
Fatin Ishraq Prapya <prapyabakshi123@gmail.com>
2023-03-14 12:55:46 +0600

    Queue Done

--- Day-2/3-Queue.py
+++ Day-2/3-Queue.py
@@ -1,34 +1,41 @@
 class Queue:
-    def __init__(self, size) -> None:
-        self.__items = [0] * size
-        self.__max_size = size
-        self.head, self.tail, self.size = 0, 0, 0
+    def __init__(self, max_size) -> None:
+        self.__items = [0] * max_size
+        self.__max_size = max_size
+        self.size = 0
+        self.head = 0
+        self.tail = 0
 
     def enqueue (self, item):
-        if self.__isFull() == True:
+        if self.__isFull():
             print("Queue is full")
             return
-        print("Inserting Item")
         self.__items[self.tail] = item
-        self.tail = (self.tail + 1) % self.__max_size
+        self.tail = (self.tail+1) % self.__max_size
         self.size += 1
-        print(self.tail)
 
     def dequeue (self):
-        pass
+        item = self.__items[self.head]
+        self.head = (self.head+1) % self.__max_size
+        self.size -= 1
+        return item
 
     def __isFull (self):
-        if self.size == self.__max_size: return True
+        if self.__max_size == self.size:
+            return True
         return False
     
-    def printQueue (self):
-        print(self.__items)
+    def isEmpty (self):
+        if self.size == 0:
+            return True
+        return False
     
 if __name__ == "__main__":
     q = Queue(5)
     q.enqueue("Fatin")
     q.enqueue("Ishraq")
     q.enqueue("Prapya")
-    q.enqueue("Carnia")
-    q.enqueue("Abu Bakkar")
-    q.printQueue()
+
+    while not q.isEmpty():
+        person = q.dequeue()
+        print(person)
